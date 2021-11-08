bcdb10567f28f514f3c3d99a97e6f87f8a149306
Drinkingbear996 <552461073@qq.com>
2021-11-08 20:34:13 +0800

    2021-11-08 20:33:45

--- src/数据结构/二叉树/HeroNode.java
+++ src/数据结构/二叉树/HeroNode.java
@@ -52,6 +52,7 @@ public class HeroNode {
                 '}';
     }
 
+
     public void PreOrder( )
     {
         //输出父节点
@@ -63,7 +64,7 @@ public class HeroNode {
             this.left.PreOrder();
         }
         //递归右子树前序遍历
-        if (this.right!=null)
+        if (this.right !=null)
         {
             this.right.PreOrder();
         }
@@ -81,14 +82,10 @@ public class HeroNode {
         System.out.println(this);
 
         //递归向右子树中序遍历
-            if (this.right.right!=null)
+            if (this.right!=null)
             {
                 this.right.infixOrder();
             }
-
-
-
-
 
     }
 
@@ -110,13 +107,148 @@ public class HeroNode {
 
     }
 
+    //二叉树  查找元素
+
+    /** 前序查找
+     * 1. 先判断当前节点是否为目标元素，如果是，则递归 如果相等，则返回当前节点
+     * 2.
+     *
+     * */
+
+
+    /**
+     * @param
+     * @param no
+     * */
+
+    public HeroNode PreOrderSearch(int no)
+    {
+        //结果节点 初始化为null
+
+        HeroNode resultNode=null;
+
+        //判断当前节点是不是空 (当前节点是root)
+        if (this.no==no)
+        {
+            return this;
+        }
+
+            //判断当前节点的左子点是否为空，如果不为空，则递归前序查找
+            //如果找到，则返回节点
+            if (this.left!=null)
+            {
+              resultNode=this.left.PreOrderSearch(no);
+            }
+
+            if (resultNode!=null) //说明我们左子树找到了
+            {
+                return resultNode;
+            }
+
+            //左子点遍历完还没找到，则进行右子节点的遍历
+            if (this.right!=null)
+            {
+               resultNode= this.right.PreOrderSearch(no);
+            }
+
+            //不管有没有找到都要返回
+           return resultNode;
+
+
+    }
+    public HeroNode InfixOrderSearch(int no)
+    {
+
+        HeroNode resultNode=null;
+
+        //判断当前节点的左子点是否为空，如果不为空，则递归前序查找
+        //如果找到，则返回节点
+
+        if (this.left!=null)
+        {
+            resultNode=this.left.InfixOrderSearch(no);
+        }
+
+
+        if (resultNode!=null)  //说明我们左子树找到了
+        {
+            return resultNode;
+        }
+
+        //判断当前节点是不是空 (当前节点是root)
+        if (this.no==no)
+        {
+            return  this;
+        }
+
+        //左子点遍历完还没找到，则进行右子节点的遍历
+        if (this.right!=null)
+        {
+            resultNode=this.right.InfixOrderSearch(no);
+        }
+
+        //不管有没有找到都要返回
+        return  resultNode;
+    }
+
+    public HeroNode PostOrderSearch(int no)
+    {
+        //这个用于接收结果节点
+        HeroNode resultNode=null;
+
+        //遍历左子树，后序遍历
+        if (this.left!=null)
+        {
+            resultNode=this.left.PostOrderSearch(no);
+        }
+        //如果左子树不为null，则证明找到了结果，返回该值
+
+        if (resultNode!=null)
+        {
+            return resultNode;
+        }
+
+        //左子树没有找到，则遍历右子树
+        if (this.right!=null)
+        {
+            resultNode=this.right.PostOrderSearch(no);
+        }
+
+        //找到了，就返回该节点
+        if (resultNode!=null)
+        {
+            return resultNode;
+        }
+
+        //如果左右节点都没有找到，就比较当前节点（root）是不是目标节点
+        if (this.no==no)
+        {
+            return this;
+        }
+        //否则仍然返回为空的结节点
+        return resultNode;
+
+
+    }
+
+
 
 }
 
 //二叉树
 class BinaryTree
 {
+
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+
     private HeroNode root;
+
+    public HeroNode getRoot() {
+        return root;
+    }
 
     public BinaryTree() {
 
@@ -156,6 +288,7 @@ class BinaryTree
     //后序遍历
     public void postOrder()
     {
+
         if (this.root!=null)
         {
             this.root.postOrder();
@@ -164,6 +297,51 @@ class BinaryTree
         {
             System.out.println("二叉树为空，无法遍历");
         }
+
     }
+
+    //前序遍历查找
+    public HeroNode PreOrderSearch(int no)
+    {
+        if (root!=null)
+        {
+            return root.PreOrderSearch(no);
+        }
+        else
+        {
+            return  null;
+        }
+
+    }
+
+    //中序遍历查找
+    public HeroNode InfixOrderSearch(int no)
+    {
+        if (root!=null)
+        {
+            return  root.InfixOrderSearch(no);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+    //后序遍历查找
+    public HeroNode PostOrderSearch(int no)
+    {
+        if (root!=null)
+        {
+            return  root.PreOrderSearch(no);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+
 
 }
---
+++ src/数据结构/二叉树/Test.java
@@ -0,0 +1,67 @@
+package 数据结构.二叉树;
+
+//前序遍历
+public class Test {
+    public static void main(String[] args) {
+
+        //创建一颗二叉树
+
+
+        HeroNode root=new HeroNode(1,"a");
+        HeroNode b=new HeroNode(2,"b");
+        HeroNode c=new HeroNode(3,"c");
+        HeroNode d=new HeroNode(4,"d");
+        HeroNode e=new HeroNode(5,"e");
+        HeroNode f=new HeroNode(6,"f");
+        HeroNode g=new HeroNode(7,"g");
+
+
+/**
+ * 手动创建二叉树
+ *        a（root）
+ *    b       c
+ *  d   e   f   g
+ * */
+
+        //说明，手动创建二叉树，后面学习递归二叉树
+
+        root.setLeft(b);
+        root.setRight(c);
+
+        b.setLeft(d);
+        b.setRight(e);
+
+        c.setLeft(f);
+        c.setRight(g);
+
+
+        BinaryTree binaryTree=new BinaryTree();
+
+        binaryTree.setRoot(root);
+
+        System.out.println("前序遍历");
+        binaryTree.preOrder();
+
+        System.out.println("中序遍历");
+        binaryTree.infixOrder();
+
+        System.out.println("后序遍历");
+        binaryTree.postOrder();
+
+
+        System.out.println("前序遍历查找");
+        HeroNode result = binaryTree.PreOrderSearch(5);
+        if (result!=null)
+        {
+            System.out.printf("找到了，信息为no=%d name=%s",result.getNo(),result.getName());
+        }
+
+        else
+        {
+            System.out.printf("没有找到 no=%d的英雄",4);
+        }
+
+
+    }
+
+}
