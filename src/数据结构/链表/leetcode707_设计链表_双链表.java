4fcece187989359c8ffe5506a018bd312f1a0b21
pioneerrr <552461073@qq.com>
2022-09-14 21:33:32 +0800

    leetcode707_设计链表（单链表和双链表）

--- src/数据结构/链表/leetcode707_设计链表_双链表.java
+++ src/数据结构/链表/leetcode707_设计链表_双链表.java
@@ -10,125 +10,92 @@ public class leetcode707_设计链表_双链表 {
 
 
 
-class node
-{
-    int val;
-    node next,prev;
-
-    node(int x)
-    {
-        val=x;
+//双链表
+class MyLinkedListed {
+    class ListNode {
+        int val;
+        ListNode next,prev;
+        ListNode(int x) {val = x;}
     }
 
-}
-class myLinkedList
-{
-//设定头结点和尾节点
-    node head,tail;
     int size;
+    ListNode head,tail;//Sentinel node
 
-    public myLinkedList()
-    {
-        size=0;
-        head=new node(0);
-        tail=new node(0);
-
-        head.next=tail;
-        tail.prev=head;
+    /** Initialize your data structure here. */
+    public MyLinkedListed() {
+        size = 0;
+        head = new ListNode(0);
+        tail = new ListNode(0);
+        head.next = tail;
+        tail.prev = head;
     }
 
-    public int get(int index)
-    {
-        if(index<0||index>=size)
-        {
-            return -1;
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        if(index < 0 || index >= size){return -1;}
+        ListNode cur = head;
+
+        // 通过判断 index < (size - 1) / 2 来决定是从头结点还是尾节点遍历，提高效率
+        if(index < (size - 1) / 2){
+            for(int i = 0; i <= index; i++){
+                cur = cur.next;
+            }
+        }else{
+            cur = tail;
+            for(int i = 0; i <= size - index - 1; i++){
+                cur = cur.prev;
+            }
         }
-        node cur=head;
-
-        // (偶数-1)/2 从头开始遍历快,(奇数-1)/2 从后面开始遍历快
-
-         if (index<(size -1 )/2)
-        {
-              for(int i=0;i<=index;i++)
-              {
-                  cur=cur.next;
-              }
-        }else
-         {
-             cur=tail;
-             for(int i=0;i<size-1-index;i++)
-             {
-                 cur=cur.prev;
-             }
-         }
-         return  cur.val;
+        return cur.val;
     }
-    public  void  addAtHead(int val)
-    {
-        node cur=head;
 
-        node newnode=new node(val);
-
-        newnode.next=cur.next;
-        cur.next=newnode;
-        cur.next.prev=newnode;
-        newnode.prev=cur;
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+        ListNode cur = head;
+        ListNode newNode = new ListNode(val);
+        newNode.next = cur.next;
+        cur.next.prev = newNode;
+        cur.next = newNode;
+        newNode.prev = cur;
         size++;
     }
 
-    public void addTail(int val)
-    {
-        node cur=tail;
-
-        node newnode=new node(val);
-        newnode.next=tail;
-        cur.prev.next=newnode;
-        newnode.prev=cur.prev;
-        cur.prev=newnode;
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        ListNode cur = tail;
+        ListNode newNode = new ListNode(val);
+        newNode.next = tail;
+        newNode.prev = cur.prev;
+        cur.prev.next = newNode;
+        cur.prev = newNode;
         size++;
-
     }
 
-    public void addAtIndex(int index,int val)
-    {
-        if(index>size)
-        {
-            return;
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if(index > size){return;}
+        if(index < 0){index = 0;}
+        ListNode cur = head;
+        for(int i = 0; i < index; i++){
+            cur = cur.next;
         }
-        if(index<0)
-        {
-            index=0;
-        }
-        node cur=head;
-
-        for(int i=0;i<index;i++)
-        {
-            cur=cur.next;
-        }
-
-        node newnode=new node(val);
-
-        newnode.next=cur.next;
-        cur.next=newnode;
-        newnode.prev=cur;
-        cur.next.prev=newnode;
+        ListNode newNode = new ListNode(val);
+        newNode.next = cur.next;
+        cur.next.prev = newNode;
+        newNode.prev = cur;
+        cur.next = newNode;
         size++;
-
     }
 
-    public void deleteAt(int index){
-        if(index>=size||index<0){
-            return;
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        if(index >= size || index < 0){return;}
+        ListNode cur = head;
+        for(int i = 0; i < index; i++){
+            cur = cur.next;
         }
-        node cur=head;
-
-        for(int i=0;i<index;i++)
-        {
-            cur=cur.next;
-        }
-
-        cur.next.next.prev=cur;
-        cur.next=cur.next.next;
+        cur.next.next.prev = cur;
+        cur.next = cur.next.next;
         size--;
     }
 }
